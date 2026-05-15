93ddff8eeed970abb4ed6f1794bfdb0625c81466
Rodrigo <rodrigovcalabria@hotmail.com>
2026-05-15 05:38:17 -0300

    setup turso db

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,14 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
+
+import { drizzle } from 'drizzle-orm/libsql';
+import { config } from 'dotenv';
 import * as schema from './schema';
 
-const sqlite = new Database('./drizzle/database.sqlite');
+config({ path: '.env.local' }); // or .env.local
 
-export const db = drizzle(sqlite, { schema });
+// const sqlite = new Database('./drizzle/database.sqlite');
+// export const db = drizzle(sqlite, { schema });
+
+export const db = drizzle({ connection: {
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+}, schema });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,17 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env.local' });
 
 export default defineConfig({
   schema: './db/schema.ts',
   out: './drizzle/migrations',
   verbose: true,
-  dialect: 'sqlite',
+  dialect: 'turso',
   dbCredentials: {
-    url: "./drizzle/database.sqlite", // Your local SQLite file
+    // url: "./drizzle/database.sqlite", // Your local SQLite file
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
 
